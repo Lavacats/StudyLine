5c8714625a6a310e8b63e7ab404903fca574f323
Lavacats <synii1212@gmail.com>
2018-11-27 13:34:24 +0900

    18.11.27
    해커랭크

---
+++ HackerRank/HakerRank_Algorithms/2018.11/18.11.26/Grading Students.cpp
@@ -0,0 +1,65 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+/*
+ * Complete the gradingStudents function below.
+ */
+vector<int> gradingStudents(vector<int> grades) {
+    /*
+     * Write your code here.
+     */
+    for(int i=0; i<grades.size();i++)
+    {
+        if(grades[i]>=38)
+        {
+            int num = 5*(grades[i]/5+1);
+            if(num-grades[i]<3)
+            {
+                grades[i] = num;
+            }
+        }
+    }
+    return grades;
+    
+}
+
+int main()
+{
+  //  ofstream fout(getenv("OUTPUT_PATH"));
+    
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    vector<int> grades(n);
+    
+    for (int grades_itr = 0; grades_itr < n; grades_itr++) {
+        int grades_item;
+        cin >> grades_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        
+        grades[grades_itr] = grades_item;
+    }
+    
+    vector<int> result = gradingStudents(grades);
+    
+    //for (int result_itr = 0; result_itr < result.size(); result_itr++) {
+    //    fout << result[result_itr];
+        
+    //    if (result_itr != result.size() - 1) {
+   //
+   //     }
+  //  }
+    
+   // fout << "\n";
+    
+  //  fout.close();
+    
+    return 0;
+}
