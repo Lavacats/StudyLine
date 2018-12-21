5e100c1fcb2a1433b13c469b8b598d0f301836f9
Ramge12 <gogo2645@naver.com>
2018-12-21 22:15:13 +0900

    18.12.21
    해커 랭크 알고리즘

---
+++ HackerRank/HakerRank_Algorithms/2018.12/12.21/Append and Delete.cpp
@@ -0,0 +1,35 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+// Complete the appendAndDelete function below.
+string appendAndDelete(string s, string t, int k) {
+	int minNum;
+	if (s.size() < t.size())minNum = s.size();
+	else minNum = t.size();
+
+	int sameNum = 0;
+	for (int i = 0; i < minNum; i++)
+	{
+		if (s[i] == t[i])sameNum = i+1;
+		else break;
+	}
+	int answer = ((s.size() - sameNum) + (t.size() - sameNum));
+	
+	if (answer > k)return "No";
+	else if (answer % 2 == k % 2)return "Yes";
+	else if (s.size() + t.size() < k)return "Yes";
+	else return "No";
+}
+int main()
+{
+	appendAndDelete("y","yu")
+}
---
+++ HackerRank/HakerRank_Algorithms/2018.12/12.21/Find Digits.cpp
@@ -0,0 +1,37 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+// Complete the findDigits function below.
+int findDigits(int n) {
+	int answer = 0;
+	int numN = n;
+	while (true)
+	{
+		int value = numN % 10;
+		if (value != 0 && n%value == 0)answer++;
+
+		numN /= 10;
+
+		if (numN < 10)
+		{
+			if (n%numN == 0)answer++;
+			break;
+		}
+	}
+	return answer;
+}
+int main()
+{
+	findDigits(12);
+
+	return 0;
+}
