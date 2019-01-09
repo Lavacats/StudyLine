a015bde876b87c7b061379817746070c1aaf79c0
Ramge12 <gogo2645@naver.com>
2019-01-09 14:07:14 +0900

    19.01.09
    해커랭크

---
+++ HackerRank/HakerRank_Algorithms/2019.01/01.09/Caesar Cipher.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <math.h>
+
+using namespace std;
+string caesarCipher(string s, int k) {
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 'a'&&s[i] <= 'z') {
+			int a = s[i] -( 97-k);
+			s[i] = (a% 26) + 97;
+		}
+		else if (s[i] >= 'A'&&s[i] <= 'Z') {
+			int a = s[i] - (65 - k);
+			s[i] = (a % 26) + 65;
+		}
+	}
+	return s;
+}
+
+int main()
+{
+	caesarCipher("Ciphering.",26);
+	return 0;
+}
