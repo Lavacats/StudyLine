4390b99a971cd96f5644605713a9f89df332a030
Ramge12 <gogo2645@naver.com>
2019-01-09 20:56:07 +0900

    19.01.09
    해커 랭크 알고리즘

---
+++ HackerRank/HakerRank_Algorithms/2019.01/01.09/HackerRank in a String!.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <math.h>
+
+using namespace std;
+
+string hackerrankInString(string s) {
+	string find = "hackerrank";
+	int stringCount = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (find[stringCount] == s[i]) {
+			stringCount++;
+		}
+	}
+	if(stringCount == find.size())return "YES";
+	else "NO";
+}
+
+
+int main()
+{
+	hackerrankInString("hackerrank.");
+	return 0;
+}
---
+++ HackerRank/HakerRank_Algorithms/2019.01/01.09/Mars Exploration.cpp
@@ -0,0 +1,33 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <math.h>
+
+using namespace std;
+
+int marsExploration(string s) {
+	int count = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (i % 3 == 0) {
+			if (s[i] != 'S')count++;
+		}
+		else if (i % 3 == 1) {
+			if (s[i] != 'O')count++;
+		}
+		else if (i % 3 == 2) {
+			if (s[i] != 'S')count++;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+	caesarCipher("Ciphering.",26);
+	return 0;
+}
