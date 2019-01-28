cfce13fe340672ab921d65b452b0e3f8427eed64
Ramge12 <gogo2645@naver.com>
2019-01-28 22:00:15 +0900

    19.01.28
    해커랭크 알고리즘

---
+++ HackerRank/HakerRank_Algorithms/2019.01/01.28/Minimum Absolute Difference in an Array.cpp
@@ -0,0 +1,29 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <list>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int t = 0;
+	cin >> t;
+	vector<int> answer;
+	for (int i = 0; i < t; i++) {
+		int num = 0;
+		cin >> num;
+		answer.push_back(num);
+	}
+	sort(answer.begin(), answer.end());
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i];
+		cout << " ";
+	}
+
+	return 0;
+}
