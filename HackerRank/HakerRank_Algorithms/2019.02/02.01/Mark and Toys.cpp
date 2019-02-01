2a968dcd4272bc4f43001cbabd4e1677a7442e1e
Ramge12 <gogo2645@naver.com>
2019-02-01 17:31:17 +0900

    19.02.01
    해커 랭크 알고리즘

---
+++ HackerRank/HakerRank_Algorithms/2019.02/02.01/Mark and Toys.cpp
@@ -0,0 +1,37 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <list>
+#include <math.h>
+using namespace std;
+
+int maximumToys(vector<int> prices, int k) {
+	sort(prices.begin(), prices.end());
+	int count = 0;
+	int answer = 0;
+	for (int i = 0; i < prices.size(); i++) {
+		if (k >= prices[i]) {
+			if (count + prices[i] <= k) {
+				answer++;
+				count += prices[i];
+			}
+			else{
+				break;
+			}
+		}
+		else {
+			break;
+		}
+	}
+	return answer;
+}
+int main()
+{
+	maximumToys({ 1,2,3,4 }, 7);
+	return 0;
+}
