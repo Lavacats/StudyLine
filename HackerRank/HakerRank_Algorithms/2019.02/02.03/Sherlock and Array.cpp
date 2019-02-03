1bbe3708f7127729e87c1279cdb64d44b3e8b15f
Ramge12 <gogo2645@naver.com>
2019-02-03 18:33:58 +0900

    19.02.03
    해커 랭크 알고리즘

---
+++ HackerRank/HakerRank_Algorithms/2019.02/02.03/Sherlock and Array.cpp
@@ -0,0 +1,38 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <list>
+#include <math.h>
+using namespace std;
+
+string balancedSums(vector<int> arr) {
+	int totalSum = 0;
+	for (int i = 0; i < arr.size(); i++) {
+		totalSum += arr[i];
+	}
+	int leftValue = 0;
+	int rightValue = totalSum;
+	int answer = -1;
+	for (int i = 0; i < arr.size(); i++) {
+		rightValue -= arr[i];
+		if (leftValue == rightValue) {
+			answer = i;
+			break;
+		}
+		leftValue += arr[i];
+	}
+	if (answer == -1)return "No";
+	else return "Yes";
+}
+
+
+int main()
+{
+
+	return 0;
+}
