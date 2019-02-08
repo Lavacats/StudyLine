9c5196bd314ff6f19292e4bf50618d05918651e2
Ramge12 <gogo2645@naver.com>
2019-02-08 22:02:21 +0900

    19.02.08
    해커 랭크 알고리즘

---
+++ HackerRank/HakerRank_Algorithms/2019.02/02.08/Tower Breakers .cpp
@@ -0,0 +1,27 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <list>
+#include <math.h>
+using namespace std;
+
+int towerBreakers(int n, int m) {
+	if (m == 1)
+		return 2;
+	if (n % 2 == 0)
+		return 1;
+	else
+		return 2;
+}
+
+
+int main()
+{
+
+	return 0;
+}
