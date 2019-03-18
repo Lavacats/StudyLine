e8f562475cbfb30b2d2da81fcddc7046012de3bd
Ramge12 <gogo2645@naver.com>
2019-03-18 22:32:53 +0900

    19.03.18
    언리얼 포트폴리오 연습

---
+++ portfolio Practice/Day37/Source/ZeldaBOTW/Player/PlayerInventory.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayerInventory.h"
+#include "Player/PlayerCharacter.h"
+#include "Player/Controller/PlayerCharacterController.h"
+#include "Player/PlayerUIWidget.h"
+
+UPlayerInventory::UPlayerInventory()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	PlayerMoney = 0;
+}
+
+void UPlayerInventory::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+void UPlayerInventory::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UPlayerInventory::AddSwordInventory(AWeapon_Equip * sword)
+{
+	Weapon_SwordList.Add(sword);
+	
+}
+
+void UPlayerInventory::AddBowdInventory(AWeapon_Equip * Bow)
+{
+	Weapon_BowList.Add(Bow);
+}
+
+void UPlayerInventory::AddShieldInventory(AWeapon_Equip * Shield)
+{
+	Weapon_ShieldList.Add(Shield);
+}
+
+void UPlayerInventory::AddFoodInventory(AItemClassFood * Food)
+{
+	Item_FoodList.Add(Food);
+}
+
+void UPlayerInventory::AddMaterialInventory(AItemClassMaterial * Material)
+{
+	Item_MaterialList.Add(Material);
+}
+
