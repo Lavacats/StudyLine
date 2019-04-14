5906e2fcf0e7a3dc4c1a87b682a94cc02f7c5f0a
Ramge12 <gogo2645@naver.com>
2019-04-14 19:36:58 +0900

    19.04.14
    해커랭크 알고리즘 연습

---
+++ HackerRank/HakerRank_Algorithms/2019.04/04.14/Priyanka and Toys.cpp
@@ -0,0 +1,35 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <list>
+#include <math.h>
+using namespace std;
+int toys(vector<int> w) {
+	int count = 0;
+	sort(w.begin(), w.end());
+	for (int i = 0; i < w.size(); i++) {
+		if (w[i] != -1) {
+			int value = w[i];
+			for (int j = i ; j < w.size(); j++) {
+				int anwer = w[j] - value;
+				if (anwer <= 4 && anwer>=0) {
+					w[j] = -1;
+				}
+			}
+			count++;
+		}
+	}
+	return count;
+}
+int main()
+{
+	vector<int> number;
+	number = { 12,15,7,8,19,24 };
+	toys(number);
+}
+
