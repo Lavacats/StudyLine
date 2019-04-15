e16dc3f959875add6476ce87b0907d48009dea39
Ramge12 <gogo2645@naver.com>
2019-04-15 20:27:25 +0900

    19.04.15
    해커 랭크 알고리즘

---
+++ HackerRank/HakerRank_Algorithms/2019.04/04.15/String Construction.cpp
@@ -0,0 +1,34 @@
+﻿// ConsoleApplication3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <list>
+#include <math.h>
+using namespace std;
+
+int stringConstruction(string s) {
+	sort(s.begin(), s.end());
+	char curChar = s[0];
+	int count = 1;
+	for (int i = 0; i < s.size(); i++) {
+		if (curChar != s[i]) {
+			count++;
+			curChar = s[i];
+		}
+	}
+	return count;
+}
+
+
+int main()
+{
+
+	stringConstruction("aabb");
+
+	return 0;
+}
+
