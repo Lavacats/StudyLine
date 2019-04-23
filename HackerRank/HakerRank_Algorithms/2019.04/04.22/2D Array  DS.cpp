18b022ce0c130297b562def32255b33a3c96d425
Ramge12 <gogo2645@naver.com>
2019-04-23 21:43:03 +0900

    해커랭크 알고리즘
    19.04.23

--- HackerRank/HakerRank_Algorithms/2019.04/04.22/2D Array  DS.cpp
+++ HackerRank/HakerRank_Algorithms/2019.04/04.22/2D Array  DS.cpp
@@ -10,21 +10,10 @@
 #include <math.h>
 using namespace std;
 
-int hourglassSum(vector<vector<int>> arr) {
-	int answer = -100;
-	// 배열값은 -9보다 작을 수 없고, 따라서 모래시계 모양의 합은 -63 보다 작을 수없다
-	for (int i = 0; i < arr.size() - 2; i++) {
-		for (int j = 0; j < arr[i].size() - 2; j++) {
-			int arrSumValue = 0;
-			arrSumValue += arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
-			arrSumValue += arr[i + 1][j + 1];
-			arrSumValue += arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
-			// 모래시계 모양의 값의 합
-			if (answer < arrSumValue) {
-				answer = arrSumValue;
-				// answer은 처음에 최소값을 가지며, 큰 값이 나올 때마다 값을 교체합니다.
-			}
-		}
+vector<int> reverseArray(vector<int> a) {
+	vector<int> answer;
+	for (int i = 0; i < a.size(); i++) {
+		answer.push_back(a[a.size() - i-1]);
 	}
 	return answer;
 }
