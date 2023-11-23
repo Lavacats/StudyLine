4ca17787adda399f5227caca67eb754057582c0d
Lavacats <synii1212@gmail.com>
2023-11-23 14:18:35 +0900

    Update 기지국설치.cpp

--- programmers/Level_3/기지국설치.cpp
+++ programmers/Level_3/기지국설치.cpp
@@ -1,9 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
 
 int solution(int n, vector<int> stations, int w)
 {
     int answer = 0;
     int index=0;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         if (index < stations.size() && i >= stations[index] - w)
         {
